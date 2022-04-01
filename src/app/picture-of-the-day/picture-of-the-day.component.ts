19de11f6adaec9b1db42072c8bc654bb67ce88b3
jkarli <jmkarli6319@gmail.com>
2022-04-01 17:58:55 -0400

    Added video support for POTD

--- src/app/picture-of-the-day/picture-of-the-day.component.ts
+++ src/app/picture-of-the-day/picture-of-the-day.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { POTD } from 'src/models/POTD';
 import { NasaService } from '../nasa.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-picture-of-the-day',
@@ -9,12 +10,13 @@ import { NasaService } from '../nasa.service';
 })
 export class PictureOfTheDayComponent implements OnInit {
 
-  constructor(public nasa: NasaService) { 
+  constructor(public nasa: NasaService, private _sanitizer: DomSanitizer) { 
 
   }
 
   public potd?: POTD;
   public currentDate: string = (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().slice(0, 10);
+  POTDVidUrl?: SafeResourceUrl;
 
   ngOnInit(): void {
     this.nasa.get_POTD().subscribe(potd =>
@@ -30,9 +32,10 @@ export class PictureOfTheDayComponent implements OnInit {
     if(date.length > 0) {
       this.nasa.get_POTD_date(date).subscribe(potd =>
       {
-        if(potd.media_type !== "video") {
-          this.potd = potd;
+        if(potd.media_type == 'video') {
+          this.POTDVidUrl = this._sanitizer.bypassSecurityTrustResourceUrl(potd.url);
         }
+        this.potd = potd;
       }, 
       error => {
         alert(error);
