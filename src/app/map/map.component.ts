c2fab2cc062181b5e9ff2a3c3b1d874af7533792
Codenameaidan <ajw137@pitt.edu>
2022-04-02 15:58:01 -0400

    Added basic map filtering

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -8,6 +8,9 @@ import { AppFilterComponent } from '../app-filter/app-filter.component';
 import { NasaService } from '../nasa.service';
 import { SpaceXService } from '../space-x.service';
 
+
+//import { firstValueFrom } from 'rxjs';
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -26,8 +29,18 @@ export class MapComponent implements OnInit {
   public observatories?: ObservatoryResponse;
   public observatory_Locations?: Observatory_with_location[];
 
-  ngOnInit(): void {
+  //For displaying : use the filtered objects!
+  public filtered_observatories?: Observatory_with_location[];
+  public filtered_launchpads?: SpaceXLaunchpad[];
+  public filtered_starlinks?: SpaceXStarLink[];
 
+  //What should be displayed?
+  public showLaunchpads: boolean = true;
+  public showStarlink: boolean = true;
+  public showNasa: boolean = true;
+
+  ngOnInit(): void {
+    /*
     //Here's how you can get all the launchpads
     this.spaceX.get_all_launchpads().subscribe(l =>
     {
@@ -67,6 +80,49 @@ export class MapComponent implements OnInit {
       alert(error);
       
     })
+    */
+    this.update_everything();
+  }
+
+  async update_everything(){
+      let response = await this.spaceX.get_all_launchpads().toPromise();
+      this.launchpads = response;
+      
+      response = await this.spaceX.get_all_starlink().toPromise();
+      this.starlinks= response;
+
+      let response2 = await this.nasa.get_observatories().toPromise();
+      response2 = response2.filter(x=>new Date(x.EndTime[1]) > new Date())
+      this.observatory_Locations = await this.nasa.get_locations(response2).toPromise();
+
+      this.filtered_launchpads = this.launchpads;
+      this.filtered_observatories = this.observatory_Locations;
+      this.filtered_starlinks = this.starlinks;
+  }
+
+  async filter(){
+    //await this.update_everything();
+
+    //Record whether checkboxes are selected
+    this.showLaunchpads = (<HTMLInputElement>document.getElementById("spacex-launchpads")).checked;
+    this.showStarlink = (<HTMLInputElement>document.getElementById("spacex")).checked;
+    this.showNasa = (<HTMLInputElement>document.getElementById("nasa")).checked;
+
+    //Filter by name (ID in some situations)
+    let name = (<HTMLInputElement>document.getElementById("name")).value;
+    if(name != null){
+      if(this.launchpads)
+        this.filtered_launchpads = this.launchpads.filter(item => item!.name!.includes(name));
+      if(this.observatory_Locations)
+        this.filtered_observatories = this.observatory_Locations.filter(item => item!.id!.includes(name));
+      if(this.starlinks)
+        this.filtered_starlinks = this.starlinks.filter(item => item!.id!.includes(name));
+    }else{
+      this.filtered_launchpads = this.launchpads;
+      this.filtered_observatories = this.observatory_Locations;
+      this.filtered_starlinks = this.starlinks;
+    }
+    
   }
 
 }
