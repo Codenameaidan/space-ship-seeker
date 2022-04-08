c72a3c1d637d5cfe65f7a645a51b6ea9bf8cbcb3
jkarli <jmkarli6319@gmail.com>
2022-04-08 00:11:27 -0400

    Set up structure for home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { POTD } from 'src/models/POTD';
+import { NasaService } from '../nasa.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public nasa: NasaService, private _sanitizer: DomSanitizer) { }
+
+  public potd?: POTD;
+  public POTDVidUrl?: SafeResourceUrl;
 
   ngOnInit(): void {
+    this.nasa.get_POTD().subscribe(potd =>
+      {
+        this.potd = potd;
+        if(potd.media_type == 'video') {
+          this.POTDVidUrl = this._sanitizer.bypassSecurityTrustResourceUrl(potd.url);
+        }
+      }, 
+      error => {
+        alert(error);
+      })
   }
 
 }
