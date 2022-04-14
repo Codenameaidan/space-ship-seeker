f780b1cf47ef5a494515eb2f625a35b2693c5565
Peiyi Wu <pwggywu226@gmail.com>
2022-04-14 19:54:15 -0400

    then issue

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,10 +22,13 @@ export class HomeComponent implements OnInit {
 
   public days?:any;
   public seconds?:any;
-  public hours?:any;
+  public hour?:any;
   public minutes?:any;
-  public address?:any;
+  public name?:any;
+  public currentTimeInSeconds?:any;
+  public timeleft?:any;
 
+  
   ngOnInit(): void {
     this.nasa.get_POTD().subscribe(potd =>
       {
@@ -38,12 +41,25 @@ export class HomeComponent implements OnInit {
         alert(error); 
       })
 
-      this.getData();//.subscribe(x => { this.getData(); });;
-    
-
-
+   
+      
+    this.getData();//.subscribe(x => { this.getData(); });;
+    console.log(this.timeleft); // not work
+    setInterval(this.countDown, 1000);
       
 
+  }
+
+  countDown(){
+    this.timeleft -= 1;
+    this.days = Math.floor(this.timeleft / ( 60 * 60 * 24));
+    this.hour = Math.floor(this.timeleft / 3600);
+    this.minutes = Math.floor(this.timeleft / 60 % 60);
+    this.seconds = Math.floor(this.timeleft % 60);
+    if(this.timeleft == 0){
+        this.getData();
+    }
+    console.log(this.timeleft);// not work
   }
   
   
@@ -58,22 +74,18 @@ export class HomeComponent implements OnInit {
         throw new Error(`Error! status: ${response.status}`);
       }
   
+      
       //console.log(response.json());
       response.json().then(data => {
         console.log(data);
         let day = data[0]['date_unix'];
         var currentTimeInSeconds=Math.floor(Date.now()/1000);
-        let timeleft = day - currentTimeInSeconds;
-        this.days = Math.floor(timeleft / ( 60 * 60 * 24));
-        this.hours = Math.floor(timeleft / 3600);
-        this.minutes = Math.floor(timeleft / 60 % 60);
-        this.seconds = Math.floor(timeleft % 60);
-
+        this.timeleft = day - currentTimeInSeconds;
+        this.name = data[0]['name'];
+        console.log(this.timeleft); // work
       })
       
-      
-      
-  
+      console.log(this.timeleft); // not
       return response;
     } catch (error) {
       if (error instanceof Error) {
@@ -89,7 +101,9 @@ export class HomeComponent implements OnInit {
   
   
 }
-
+function delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
+}
 
 
 
